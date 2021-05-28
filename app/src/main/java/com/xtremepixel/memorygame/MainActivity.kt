dd8642bf30e9ddf5ae2708a50c10495bda906148
Sam Esidem <samesidem@gmail.com>
2021-05-28 17:59:03 +0100

    Recycler View setup complete

--- app/src/main/java/com/xtremepixel/memorygame/MainActivity.kt
+++ app/src/main/java/com/xtremepixel/memorygame/MainActivity.kt
@@ -3,13 +3,16 @@ package com.xtremepixel.memorygame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.xtremepixel.memorygame.models.BoardSize
 
 class MainActivity : AppCompatActivity() {
 
      private lateinit var movesText: TextView
      private lateinit var pairsText: TextView
      private lateinit var recyclerView:RecyclerView
+     private var  boardsize : BoardSize = BoardSize.HARD
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +20,9 @@ class MainActivity : AppCompatActivity() {
         movesText = findViewById(R.id.moves)
         pairsText = findViewById(R.id.pairs)
         recyclerView = findViewById(R.id.recyclerViewBoard)
+
+        recyclerView.layoutManager = GridLayoutManager(this, boardsize.getWidth())
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = MemoryGameAdapter(this, boardsize)
     }
 }
---
+++ app/src/main/java/com/xtremepixel/memorygame/MemoryGameAdapter.kt
@@ -0,0 +1,55 @@
+package com.xtremepixel.memorygame
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import com.xtremepixel.memorygame.models.BoardSize
+import kotlin.math.min
+
+class MemoryGameAdapter(private var context: Context, private var cardCount: BoardSize) :
+    RecyclerView.Adapter<MemoryGameAdapter.ViewHolder>() {
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        var cardWidth = parent.width/cardCount.getWidth() - (2* MARGIN)
+        var cardHeight = parent.height/cardCount.getHeight() - (2* MARGIN)
+        var cardSideLength = min(cardWidth,cardHeight)
+       var view = LayoutInflater.from(context).inflate(R.layout.card_layout,parent,false)
+
+        var layoutParm = view.findViewById<View>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParm.width = cardSideLength
+        layoutParm.height = cardSideLength
+        layoutParm.setMargins(MARGIN, MARGIN, MARGIN, MARGIN)
+
+    return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = cardCount.numCard
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private val  imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+        fun bind(position: Int) {
+           imageButton.setOnClickListener {
+               Log.i(TAG, "Clicked on position $position")
+           }
+        }
+    }
+
+    companion object{
+
+        private const val MARGIN = 10
+        private const val  TAG = "Memory Game Adapter"
+    }
+}
