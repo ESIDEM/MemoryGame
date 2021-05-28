33778a7e0e45cea1d3d1792dfc2eb78fc3a20e17
Sam Esidem <samesidem@gmail.com>
2021-05-28 14:58:41 +0100

    Home Screen setup complete

--- app/src/main/java/com/xtremepixel/memorygame/MainActivity.kt
+++ app/src/main/java/com/xtremepixel/memorygame/MainActivity.kt
@@ -2,10 +2,20 @@ package com.xtremepixel.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+     private lateinit var movesText: TextView
+     private lateinit var pairsText: TextView
+     private lateinit var recyclerView:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        movesText = findViewById(R.id.moves)
+        pairsText = findViewById(R.id.pairs)
+        recyclerView = findViewById(R.id.recyclerViewBoard)
     }
 }
